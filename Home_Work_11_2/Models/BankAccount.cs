6f3248fcd43f1e0910fd28c525ab766e22b76ea7
Puncker1978 <nvv197827@gmail.com>
2023-10-03 02:20:36 +0500

    1

--- Home_Work_11_2/Models/BankAccount.cs
+++ Home_Work_11_2/Models/BankAccount.cs
@@ -14,45 +14,59 @@
         private decimal sum;
 
         /// <summary>
+        /// Номер счёта
+        /// </summary>
+        internal Guid Id { get => id; set => id = value; }
+
+        /// <summary>
         /// Сумма на счёте
         /// </summary>
-        public Guid Id { get; set; }
+        public decimal Sum { get => sum; set => sum = value; }
 
-        public BankAccount(int sum)
+        /// <summary>
+        /// Конструктор для создания нового банковского счёта
+        /// </summary>
+        /// <param name="sum">Начальная сумма на счёте</param>
+        internal BankAccount(decimal sum)
         {
             this.Id = Guid.NewGuid();
-            this.sum = sum;
+            this.Sum = sum;
         }
 
         // Создаем переменную делегата
         AccountHandler? taken;
 
-
         // Регистрируем делегат
-        public void RegisterHandler(AccountHandler del) => taken = del;
+        internal void RegisterHandler(AccountHandler del) => taken = del;
 
         /// <summary>
         /// Метод для пополения баланса банковского счёта
         /// </summary>
-        /// <param name="sum"></param>
-        public void TopUpAccount(int sum) => this.sum += sum;
+        /// <param name="sum">Сумма пополения счёта</param>
+        internal void TopUpAccount(decimal sum) => this.Sum += sum;
 
         /// <summary>
         /// Метод снятия с банковского счёта
         /// </summary>
-        /// <param name="sum"></param>
-        public void TopDownAccount(int sum)
+        /// <param name="sum">Сумма снятия со счёта</param>
+        internal void TopDownAccount(decimal sum)
         {
-            if (this.sum >= sum)
+            if (this.Sum >= sum)
             {
-                this.sum -= sum;
+                this.Sum -= sum;
                 // вызываем делегат, передавая ему сообщение
                 taken?.Invoke($"Со счета списано {sum} у.е.");
             }
             else
             {
-                taken?.Invoke($"Недостаточно средств. Баланс: {this.sum} у.е.");
+                taken?.Invoke($"Недостаточно средств. Баланс: {this.Sum} у.е.");
             }
         }
+
+        /// <summary>
+        /// Метод возвращает баланс на счету
+        /// </summary>
+        /// <returns>Баланс счёта</returns>
+        internal decimal ShowBalance() => this.Sum;
     }
 }
