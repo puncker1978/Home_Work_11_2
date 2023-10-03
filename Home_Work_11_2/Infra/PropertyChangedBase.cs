5c2fab3b36f530201bf709fd35c13dc4117342e2
Puncker1978 <nvv197827@gmail.com>
2023-10-03 21:19:49 +0500

    1

---
+++ Home_Work_11_2/Infra/PropertyChangedBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Home_Work_11_2.Infra
+{
+    internal class PropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
