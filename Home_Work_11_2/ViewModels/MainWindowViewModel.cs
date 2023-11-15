7db8747d857c77b3d688c1d1e4e65cfa38609443
Puncker1978 <nvv197827@gmail.com>
2023-11-16 00:04:40 +0500

    1

--- Home_Work_11_2/ViewModels/MainWindowViewModel.cs
+++ Home_Work_11_2/ViewModels/MainWindowViewModel.cs
@@ -11,27 +11,35 @@ namespace Home_Work_11_2.ViewModels
 {
     internal class MainWindowViewModel : PropertyChangedBase
     {
+        #region Поля
         public static bool Flag => Position == "Менеджер";
-        public static string? Position {  get; set; }
-        
-        public static Client? SelectedClient {  get; set; }
+        #endregion
 
+        #region Свойства
+        public static string? Position { get; set; }
+        public static Client? SelectedClient { get; set; }
+        public IEnumerable<Client> Clients { get; set; }
+        #endregion
+
+        #region Конструкторы
         public MainWindowViewModel(string position)
         {
             Clients = Repository.GetClients();
             Position = position;
-            ShowWindowCommand = new RelayCommand(ShowWindow, CanShowWindow);
+            ShowAddNewClientWindowCommand = new RelayCommand(ShowWindow, CanShowWindow);
+            DeleteClientCommand = new RelayCommand(DeleteClient, CanDeleteClient);
+            ShowEditClientWindowCommand = new RelayCommand(ShowEditClientWindow, CanShowEditClientWindow);
         }
+        #endregion
 
-        public ObservableCollection<Client> Clients { get; set; }
-
-        public ICommand ShowWindowCommand { get; set; }
-
+        #region Команды
+        #region Команда открытия окна для добавления нового клиента
+        //Команда открытия окна для добавления нового клиента
+        public ICommand ShowAddNewClientWindowCommand { get; set; }
         private bool CanShowWindow(object obj)
         {
             return Position == "Менеджер";
         }
-
         private void ShowWindow(object obj)
         {
             var mainWindow = obj as Window;
@@ -43,5 +51,40 @@ namespace Home_Work_11_2.ViewModels
             };
             newClientWindow.ShowDialog();
         }
+        #endregion
+
+        #region Команда открытия окна для редактирования клиента
+        //Команда открытия окна для редактирования клиента
+        public ICommand ShowEditClientWindowCommand { get; set; }
+        private bool CanShowEditClientWindow(object obj)
+        {
+            return true;
+        }
+        private void ShowEditClientWindow(object obj)
+        {
+            var mainWindow = obj as Window;
+
+            EditClientWindow editClientWindow = new()
+            {
+                Owner = mainWindow,
+                WindowStartupLocation = WindowStartupLocation.CenterScreen
+            };
+            editClientWindow.ShowDialog();
+        }
+        #endregion
+
+        #region Команда для удаления клиента
+        //Команда для удаления клиента
+        public ICommand DeleteClientCommand { get; set; }
+        private bool CanDeleteClient(object obj)
+        {
+            return SelectedClient != null;
+        }
+        private void DeleteClient(object obj)
+        {
+            Repository.RemoveClient(SelectedClient);
+        }
+        #endregion
+        #endregion Команды
     }
 }
