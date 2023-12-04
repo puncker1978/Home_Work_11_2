47531d3eeabc9caab7db6a16ba6f361a4f499c6e
Puncker1978 <nvv197827@gmail.com>
2023-12-04 22:05:14 +0500

    1

---
+++ ReflectionSort/Program.cs
@@ -0,0 +1,36 @@
+﻿using static ReflectionSort.Repository;
+
+namespace ReflectionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (Person person in Persons)
+            {
+                Console.WriteLine($"{person.FirstName}\t {person.LastName}\t {person.Age}");
+            }
+            Console.WriteLine();
+
+
+
+            Type type = typeof(Person);
+            var prop1 = type.GetProperty("LastName");
+            var prop2 = type.GetProperty("FirstName");
+
+
+            List<Person> sortedPersons = (from person in Persons
+                                          orderby person.GetType().GetProperty("LastName").Name
+                                          select person) as List<Person>;
+
+
+            foreach (Person person in sortedPersons)
+            {
+                Console.WriteLine($"{person.FirstName}\t {person.LastName}\t {person.Age}");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReflectionSort/Repository.cs
@@ -0,0 +1,15 @@
+﻿namespace ReflectionSort
+{
+    internal class Repository
+    {
+        public static List<Person> Persons =
+           [
+           new Person("Иван", "Николаев", 26),
+            new Person("Иван", "Степанов", 21),
+            new Person("Анна", "Николаева", 24),
+            new Person("Анна", "Владимирова", 28),
+            new Person("Борис", "Корчевников", 20),
+            new Person("Борис", "Инжуватов", 27),
+            ];
+    }
+}
